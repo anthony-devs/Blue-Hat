afe9abae27269819cac76dce3807e00450c2ba92
Anthony Somtochukwu <122059976+anthony-devs@users.noreply.github.com>
2023-03-07 10:58:57 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ import setuptools
 
 
 setuptools.setup(
-    name="bi6",
+    name="blueHat",
     version="0.0.1",
     author="Anthony Somtochukwu",
     author_email="somtochukwuanthony460@gmail.com",
     description=(" A Hacking Tool "
                 "For all hackers."),
-    url="https://github.com/anthony-devs/bi6/",
+    url="https://github.com/anthony-devs/blue-hat/",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
@@ -18,7 +18,7 @@ setuptools.setup(
     python_requires=">=3.6",
     entry_points={
         "console_scripts": [
-            "bi6 = BI6.cli:main",
+            "blueHat = BI6.cli:main",
         ]
     }
 )
